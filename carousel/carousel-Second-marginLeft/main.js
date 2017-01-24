9dfabaabb2db84b986e8145a40bcffc8f788f32b
Sergey Koshman <koshman.animys@gmail.com>
2017-01-24 14:45:17 +0200

    carousel - add second variant with margin-left

---
+++ carousel/carousel-Second-marginLeft/main.js
@@ -0,0 +1,38 @@
+'use strict'
+
+var next = document.getElementById('next');
+var prev = document.getElementById('prev');
+
+var itemList = document.getElementById('itemList');
+var listStyle = getComputedStyle(itemList);
+
+var item = document.querySelector('li');
+
+next.addEventListener('click', moveNext);
+prev.addEventListener('click', movePrev);
+
+function moveNext() {
+  var k = counterMargin();
+
+  if (k === 0) {
+    var newMargin = k - item.offsetWidth;
+  } else if (k === -7) {
+    return;
+  } else {
+    var newMargin = (k-1)*item.offsetWidth;
+  }
+  itemList.style.marginLeft = newMargin + 'px';
+}
+
+function movePrev() {
+  if (parseInt(listStyle.marginLeft) === 0) {
+    return console.log('edge');
+  } else {
+      itemList.style.marginLeft = parseInt(listStyle.marginLeft) + item.offsetWidth + 'px';
+  }
+}
+
+function counterMargin() {
+  var marginLeftAlredy = parseInt(listStyle.marginLeft);
+  return marginLeftAlredy/item.offsetWidth;
+}
