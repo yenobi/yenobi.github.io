a42bd596cae8f81c7c94369da12344477c03c36a
Sergey Koshman <koshman.animys@gmail.com>
2017-02-09 16:03:36 +0200

    hamburger animation - finished first step to red X

--- animationHamb/main.js
+++ animationHamb/main.js
@@ -1,12 +1,26 @@
 'use strict'
 
-var wrapper = document.getElementById('wrapper');
-var hero = document.getElementById('hero');
-wrapper.addEventListener('click', addGoing);
+var container = document.getElementById('container');
+var middleDiv = document.querySelector('.middle');
 
-function addGoing() {
-  hero.classList.toggle('becomeLittle');
-  setTimeout("hero.classList.toggle('becomeLittle');", 1000);
-  setTimeout("hero.classList.toggle('goingRight');", 1000);
-  setTimeout("hero.classList.toggle('goingRight');", 3000);
+container.addEventListener('click', addGoingDiv);
+
+function addGoingDiv() {
+  middleDiv.classList.toggle('becomeLittleDiv');
+  setTimeout("middleDiv.classList.toggle('becomeLittleDiv');", 1000);
+  setTimeout("middleDiv.classList.toggle('divGoingRight');", 1000);
+}
+
+// для возвращения в первоначальное состояние, сперва
+// нужно будет удалить имеющийся класс с анимацией
+// чтобы ее новой анимацией возвращения заменить
+
+var firstDiv = document.querySelector('#container div:first-child');
+var lastDiv = document.querySelector('#container div:last-child');
+
+container.addEventListener('click', makingX);
+
+function makingX() {
+  setTimeout("firstDiv.classList.toggle('uppperToX');", 2000);
+  setTimeout("lastDiv.classList.toggle('lowerToX');", 2000);
 }
