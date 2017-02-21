08823105ddbfd0fed29605fcf8df1f0071e415e7
Sergey Koshman <koshman.animys@gmail.com>
2017-02-21 18:02:40 +0200

    cityComf - add tabs in portfolio-section

--- cityComf/js/build/build.js
+++ cityComf/js/build/build.js
@@ -13118,3 +13118,26 @@ return jQuery;
 
   });
 })();
+;(function($, undefined) {
+  var $buttons = $('#portfolio .wrapper-buttons button');
+  console.log($buttons);
+  var buttons = $.makeArray($buttons);
+  var $contents = $('.wrapper-testimonal');
+  showContent();
+
+  $('.wrapper-buttons').on('click', function(event) {
+    console.log('1');
+    showContent();
+    $('.active').toggleClass('active');
+    $(event.target).toggleClass('active');
+    showContent();
+  });
+
+  function showContent() {
+    var activeButton = $('.active')[0];
+    console.log(activeButton);
+    var index = buttons.indexOf(activeButton);
+    var $activeContent = $($contents[index]);
+    $activeContent.toggleClass('displayNone');
+  };
+})(jQuery);
